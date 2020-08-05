94dc3a0ef6b16cbb3c2452c908feb932bb61436d
Lokesh Krishna <lokesha1b2c3@gmail.com>
2020-08-05 10:56:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(name='gym_stoch2_sloped_terrain',
       version='0.0.1',
       python_requires= '~=3.6',
-      install_requires=['gym~=0.17.2','pybullet','sklearn','matplotlib','fabulous']  # And any other dependencies foo needs
+      install_requires=['gym~=0.17.2','pybullet','sklearn','matplotlib','fabulous','dataclasses']  # And any other dependencies foo needs
 )
